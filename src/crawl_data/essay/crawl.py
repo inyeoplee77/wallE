05bc1a03b38362c1e3dbea582c1fd1464edb819f
inyeoplee77 <blackin77@gmail.com>
2016-03-07 02:14:31 +0900

    crawl essay first try

---
+++ src/crawl_data/essay/crawl.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import urllib
+
+base = "http://essay.or.kr/gnu4/bbs/board.php"
+essay = "?bo_table=essay100"
+#&page=2
+def crawl_text(url,p):
+	for i in range(1,p):
+		r = urllib.urlopen(base+url+str(i)).read()
+		tds = BeautifulSoup(r).select('td[style="word-break:break-all;"]')
+		for td in tds:
+			r = urllib.urlopen(base.split('bbs')[0] + td.find('a')['href'][3:]).read()
+			text = BeautifulSoup(r).find('span','ct lh')
+			if not text:
+				continue
+			text = text.text
+			print text
+#poem
+crawl_text(essay + "&page=",7)
