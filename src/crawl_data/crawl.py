eeeea121de183554b6a4955cd9870bdaadf5684c
inyeoplee77 <blackin77@gmail.com>
2016-03-07 00:41:23 +0900

    crawl second try

--- src/crawl_data/crawl.py
+++ src/crawl_data/crawl.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
 import urllib
-r = urllib.urlopen("http://www.supil.or.kr/essay/talkbox/viewbody.html?partid=29&code=talkbox2&page=1&number=51438&keyfield=&key=").read()
 
+f = open("data_supil","w")
 number = 51438
-soup = BeautifulSoup(r).select('td[bgcolor="#FFFFFF"]')[1]
+for i in range(51438,0,-1):
+	
+	r = urllib.urlopen("http://www.supil.or.kr/essay/talkbox/viewbody.html?partid=29&code=talkbox2&page=1&number="+str(i)+"&keyfield=&key=").read()	
+	text = BeautifulSoup(r).select('td[bgcolor="#FFFFFF"]')
+	if len(text) < 1:
+		continue
+	text = text[1].text
+	if not text:
+		continue
+	f.write(text.encode('utf-8'))
 
-print soup
