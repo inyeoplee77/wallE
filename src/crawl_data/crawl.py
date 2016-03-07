a811649507d20325de82fe2dc002ad78c7ab6011
inyeoplee77 <blackin77@gmail.com>
2016-03-07 20:32:57 +0900

    fix

--- src/crawl_data/crawl.py
+++ src/crawl_data/crawl.py
@@ -1,23 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib
 
-f = open("test1.txt", "w")
+base = "http://www.essayistclub.or.kr/"
 
-
-for i in range(1, 10):
-	for j in range (1, 10):
-		r = urllib.urlopen("http://www.qtessay.or.kr/n"+str(0)+str(i)+"a"+str(0)+str(j)+".htm").read()
-		text = BeautifulSoup(r).find('table').text
-		print text
-for i in range(10, 73):
-	for j in range (10, 50):
-		r = urllib.urlopen("http://www.qtessay.or.kr/n"+str(i)+"a"+str(j)+".htm").read()
+for i in range(1,100):
+	for j in range(1,10):
+		if(i<10):
+			r = urllib.urlopen(base + '0' + str(i) + 'm' + '0' + str(j) + '.htm').read()
+		else:
+			r = urllib.urlopen(base + str(i) + 'm' + '0' + str(j) + '.htm').read()
 		b = BeautifulSoup(r)
-    	if '404' in b.find('title').text:
-      		break
-    	text = b.find('table').text
-    	if len(text)<1:
-    		continue
-    	else:
-    		print text
-
+    		if '404' in b.find('title').text:
+    			break
+    		else:
+    			text = BeautifulSoup(r).find('table').text
+			print text
