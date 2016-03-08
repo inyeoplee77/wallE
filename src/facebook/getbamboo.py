da52263434152ad3f7f08715237ede872c2ec574
inyeoplee77 <blackin77@gmail.com>
2016-03-09 05:54:09 +0900

    clustering

--- src/facebook/getbamboo.py
+++ src/facebook/getbamboo.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import requests
+import os
+import re
+import locale
 appID = '719437504828313'
 appSecret = 'dae1d765e81c02e566d6ccaa2b6af441'
 
-token = "CAAKOUxjp05kBAM1Ay3YsOAzQIUZBmTw3JOk5U8CXzTy7h1ByIdmZBOoCZBz63YFbRPewFX3Ig4wyBCohQ3zB1TppS4duqNrgS0A2ZABgXZB6CmDDBpPpeIGJtnk3VkKFU40j3YK5N3dOct3Ji1SFCtUz6jOEfNcHu4eXko8CvjH6pxIKpp73Ufx9sfq86UZBdq6RqZCSYf6DwZDZD"
+token = "CAAKOUxjp05kBAGv2nl1XKZBWBp7bfyStp2FhRTzJTV3xJZBObadaYBSDyEEGDTezs81XofaYEbPtMdcBTwYHZCi99wHjuX6VWCNJesM6jEtF6UxhtqvHIidpGBm6u4pXnDuNFwpLjLl9HEqjJwIUI0ZBN4iJx57Tzu3qdoGZCuZB6LwdAf3816"
 
 #token = "CAAKOUxjp05kBAMd0ihtkaLKrkjZAAZBkwUcSl1VOjdHypVyqgH4lIuaXjWfMdXNDgD4oEGA8FMMj8m2WjX9S9YmPOrbo67jypUki3eKtWln5wwGVMjvsb7mMLzAq8ZCTPYm7vkKm68uYJ9asTAb2JC77ntMnV8CATnGLAYLkZAjZA3nTzvOdfG4dOYMEf9LBs5C9uGYTNjwZDZD"
 
@@ -18,40 +21,53 @@ for line in f:
 	line = line.split(',')
 	pages[line[2]] = line[1]
 posts = {}
+page_errors = {}
+likes_errors = []
+likes = {}
+count = 0
 
 
-errors = {}
-
-f = open("posts",'w')
+#locale.setlocale(locale.LC_ALL,'')
+#[^ ㄱ-ㅎ|ㅏ-ㅣㅣ가-힣]+
+nonkorean = re.compile('[^ 가-힣]+')
+hashtag = re.compile('#[가-힣\w0-9\S]+')
+nomean = re.compile('[ㄱ-ㅎ]')
+print nomean.sub('','뭐야 이거')
 for page in pages:
+	dir = pages[page].split('/')[-1]
+	if not os.path.exists(dir):
+		os.makedirs(dir)
 	r = requests.get(api + page + "/posts",params = para).json()
-	posts[page] = []
-	if 'error' in r or 'paging' not in r:
-		print r.keys()
-		print "Something went wrong"
-		errors[page] = r
-	#get all posts in the page and save in following format :  (post_id,content of post)\n(post_id,content)\n .... (just for the test)
-	#
-	#gonna change the format
-	
+	if 'error' in r:
+		page_errors[page] = r['error']['message']
+		continue
 	while 'previos' in r['paging']:
 		r = requests.get(r['paging']['previous']).json()
 	while 'next' in r['paging']:
 		for p in r['data']:
-			f.write( '(' + p['id'].encode('utf-8') + ',' + p['message'].encode('utf-8') +')' + '\n')
-			#print requests.get(api + p['id'] + '/likes/' + page,params = para).json()
-			#if requests.get(api + p['id'] + '/likes/' + page,params = para).json()['data']:
-			#	print p['id'] + 'likes' + page
+			f = open(dir+ '/' + p['id'],'w')
+			if 'message' not in p:
+				continue
+			message = nomean.sub('',p['message']).encode('utf8').strip()
+			message = ' '.join(nonkorean.sub('',hashtag.sub('',message)).split())
+			f.write(message.strip())
+			count += 1
+			like = requests.get(api + p['id'] + '/likes?summary=true',params = para).json()
+			if 'error' in like:
+				likes_errors.append(p['id']) 
+				continue
+			else:
+				likes[p['id']] = like['summary']['total_count']
 		r = requests.get(r['paging']['next']).json()
-	
-
-	#people who liked the page   {user_id}/likes/{page_id} check if key 'data' empty or not
-	
-	#count the number of likes for the page
-	r = requests.get(api+page+'?fields=likes',params=para).json()['likes']
-	print r
-	
-	
+		print '%d posts scrapped' % count
+			
+for e in page_errors:
+	print 'Error occurred while processing ' + e + 'page :' + page_errors[e]
+for e in likes_errors:
+	print 'Error occurred while obtaining likes on post' + e
+f = open('likes','w')
+for like in likes:
+	f.write(like+' ' + likes[like] + '\n')
 	
 
 
