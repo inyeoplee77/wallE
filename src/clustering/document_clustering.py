b450b984351be97d637603186cad3f90c8757c46
inyeoplee77 <blackin77@gmail.com>
2016-03-09 20:12:33 +0900

    LDA

--- src/clustering/document_clustering.py
+++ src/clustering/document_clustering.py
@@ -60,12 +60,13 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
 from sklearn import metrics
 from sklearn.datasets import load_files
 from sklearn.cluster import KMeans, MiniBatchKMeans
-
+from sklearn.decomposition import NMF, LatentDirichletAllocation
 import logging
 from optparse import OptionParser
 import sys
@@ -238,3 +239,71 @@ if not opts.use_hashing:
         for ind in order_centroids[i, :10]:
             print(' %s' % terms[ind], end='')
         print()
+
+
+
+
+
+#LDA
+
+n_samples = 40
+n_features = 100
+n_topics = 10
+n_top_words = 20
+
+
+def print_top_words(model, feature_names, n_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic #%d:" % topic_idx)
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+    print()
+
+
+print("Loading dataset...")
+t0 = time()
+
+data_samples = dataset.data
+print("done in %0.3fs." % (time() - t0))
+
+# Use tf-idf features for NMF.
+print("Extracting tf-idf features for NMF...")
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=n_features,
+                                   stop_words='english')
+t0 = time()
+tfidf = tfidf_vectorizer.fit_transform(data_samples)
+print("done in %0.3fs." % (time() - t0))
+
+# Use tf (raw term count) features for LDA.
+print("Extracting tf features for LDA...")
+tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=n_features,
+                                stop_words='english')
+t0 = time()
+tf = tf_vectorizer.fit_transform(data_samples)
+print("done in %0.3fs." % (time() - t0))
+
+# Fit the NMF model
+print("Fitting the NMF model with tf-idf features,"
+      "n_samples=%d and n_features=%d..."
+      % (n_samples, n_features))
+t0 = time()
+nmf = NMF(n_components=n_topics, random_state=1, alpha=.1, l1_ratio=.5).fit(tfidf)
+
+print("done in %0.3fs." % (time() - t0))
+
+print("\nTopics in NMF model:")
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+print_top_words(nmf, tfidf_feature_names, n_top_words)
+
+print("Fitting LDA models with tf features, n_samples=%d and n_features=%d..."
+      % (n_samples, n_features))
+lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
+                                learning_method='online', learning_offset=50.,
+                                random_state=0)
+t0 = time()
+lda.fit(tf)
+print("done in %0.3fs." % (time() - t0))
+
+print("\nTopics in LDA model:")
+tf_feature_names = tf_vectorizer.get_feature_names()
+print_top_words(lda, tf_feature_names, n_top_words)
