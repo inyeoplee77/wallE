68202e7ef381e876d6c61ffdb968eeab8baf5379
inyeoplee77 <blackin77@gmail.com>
2016-03-14 17:33:13 +0900

    cluster

--- src/clustering/document_clustering.py
+++ src/clustering/document_clustering.py
@@ -11,19 +11,19 @@ a scipy.sparse matrix to store the features instead of standard numpy arrays.
 Two feature extraction methods can be used in this example:
 
   - TfidfVectorizer uses a in-memory vocabulary (a python dict) to map the most
-    frequent words to features indices and hence compute a word occurrence
-    frequency (sparse) matrix. The word frequencies are then reweighted using
-    the Inverse Document Frequency (IDF) vector collected feature-wise over
-    the corpus.
+	frequent words to features indices and hence compute a word occurrence
+	frequency (sparse) matrix. The word frequencies are then reweighted using
+	the Inverse Document Frequency (IDF) vector collected feature-wise over
+	the corpus.
 
   - HashingVectorizer hashes word occurrences to a fixed dimensional space,
-    possibly with collisions. The word count vectors are then normalized to
-    each have l2-norm equal to one (projected to the euclidean unit-ball) which
-    seems to be important for k-means to work in high dimensional space.
+	possibly with collisions. The word count vectors are then normalized to
+	each have l2-norm equal to one (projected to the euclidean unit-ball) which
+	seems to be important for k-means to work in high dimensional space.
 
-    HashingVectorizer does not provide IDF weighting as this is a stateless
-    model (the fit method does nothing). When IDF weighting is needed it can
-    be added by pipelining its output to a TfidfTransformer instance.
+	HashingVectorizer does not provide IDF weighting as this is a stateless
+	model (the fit method does nothing). When IDF weighting is needed it can
+	be added by pipelining its output to a TfidfTransformer instance.
 
 Two algorithms are demoed: ordinary k-means and its more scalable cousin
 minibatch k-means.
@@ -78,46 +78,46 @@ import numpy as np
 
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s')
+					format='%(asctime)s %(levelname)s %(message)s')
 
 # parse commandline arguments
 op = OptionParser()
 op.add_option("--lsa",
-              dest="n_components", type="int",
-              help="Preprocess documents with latent semantic analysis.")
+			  dest="n_components", type="int",
+			  help="Preprocess documents with latent semantic analysis.")
 op.add_option("--no-minibatch",
-              action="store_false", dest="minibatch", default=True,
-              help="Use ordinary k-means algorithm (in batch mode).")
+			  action="store_false", dest="minibatch", default=True,
+			  help="Use ordinary k-means algorithm (in batch mode).")
 op.add_option("--no-idf",
-              action="store_false", dest="use_idf", default=True,
-              help="Disable Inverse Document Frequency feature weighting.")
+			  action="store_false", dest="use_idf", default=True,
+			  help="Disable Inverse Document Frequency feature weighting.")
 op.add_option("--use-hashing",
-              action="store_true", default=False,
-              help="Use a hashing feature vectorizer")
+			  action="store_true", default=False,
+			  help="Use a hashing feature vectorizer")
 op.add_option("--n-features", type=int, default=10000,
-              help="Maximum number of features (dimensions)"
-                   " to extract from text.")
+			  help="Maximum number of features (dimensions)"
+				   " to extract from text.")
 op.add_option("--verbose",
-              action="store_true", dest="verbose", default=False,
-              help="Print progress reports inside k-means algorithm.")
+			  action="store_true", dest="verbose", default=False,
+			  help="Print progress reports inside k-means algorithm.")
 
 print(__doc__)
 op.print_help()
 
 (opts, args) = op.parse_args()
 if len(args) > 0:
-    op.error("this script takes no arguments.")
-    sys.exit(1)
+	op.error("this script takes no arguments.")
+	sys.exit(1)
 
 
 ###############################################################################
 # Load some categories from the training set
 '''
 categories = [
-    'alt.atheism',
-    'talk.religion.misc',
-    'comp.graphics',
-    'sci.space',
+	'alt.atheism',
+	'talk.religion.misc',
+	'comp.graphics',
+	'sci.space',
 ]
 '''
 
@@ -133,10 +133,10 @@ data_lst = list()
 target = list()
 filenames = list()
 for subset in ('train', 'test'):
-    data = cache[subset]
-    data_lst.extend(data.data)
-    target.extend(data.target)
-    filenames.extend(data.filenames)
+	data = cache[subset]
+	data_lst.extend(data.data)
+	target.extend(data.target)
+	filenames.extend(data.filenames)
 data.data = data_lst
 data.target = np.array(target)
 data.filenames = np.array(filenames)
@@ -144,29 +144,31 @@ dataset = data
 ################
 
 print("%d documents" % len(dataset.data))
-print("%d categories" % len(dataset.target_names))
+print("%d categories" % len(dataset.target))
 print()
-
-labels = dataset.target
-true_k = np.unique(labels).shape[0]
+#labels = dataset.target
+true_k = 10
+print 
+#true_k = np.unique(labels).shape[0]
 
 print("Extracting features from the training dataset using a sparse vectorizer")
 t0 = time()
 if opts.use_hashing:
-    if opts.use_idf:
-        # Perform an IDF normalization on the output of HashingVectorizer
-        hasher = HashingVectorizer(n_features=opts.n_features,
-                                    non_negative=True,
-                                   norm=None, binary=False)
-        vectorizer = make_pipeline(hasher, TfidfTransformer())
-    else:
-        vectorizer = HashingVectorizer(n_features=opts.n_features,
-                                       non_negative=False, norm='l2',
-                                       binary=False)
+	if opts.use_idf:
+		# Perform an IDF normalization on the output of HashingVectorizer
+		hasher = HashingVectorizer(n_features=opts.n_features,
+									non_negative=True,
+								   norm=None, binary=False)
+		vectorizer = make_pipeline(hasher, TfidfTransformer())
+	else:
+		vectorizer = HashingVectorizer(n_features=opts.n_features,
+									   non_negative=False, norm='l2',
+									   binary=False)
 else:
-    vectorizer = TfidfVectorizer(max_df=0.5, max_features=opts.n_features,
-                                 min_df=2,
-                                 use_idf=opts.use_idf)
+	vectorizer = TfidfVectorizer(max_df=0.5, max_features=opts.n_features,
+								 min_df=2,
+								 use_idf=opts.use_idf,
+								 decode_error = 'ignore')
 X = vectorizer.fit_transform(dataset.data)
 
 print("done in %fs" % (time() - t0))
@@ -174,87 +176,86 @@ print("n_samples: %d, n_features: %d" % X.shape)
 print()
 
 if opts.n_components:
-    print("Performing dimensionality reduction using LSA")
-    t0 = time()
-    # Vectorizer results are normalized, which makes KMeans behave as
-    # spherical k-means for better results. Since LSA/SVD results are
-    # not normalized, we have to redo the normalization.
-    svd = TruncatedSVD(opts.n_components)
-    normalizer = Normalizer(copy=False)
-    lsa = make_pipeline(svd, normalizer)
+	print("Performing dimensionality reduction using LSA")
+	t0 = time()
+	# Vectorizer results are normalized, which makes KMeans behave as
+	# spherical k-means for better results. Since LSA/SVD results are
+	# not normalized, we have to redo the normalization.
+	svd = TruncatedSVD(opts.n_components)
+	normalizer = Normalizer(copy=False)
+	lsa = make_pipeline(svd, normalizer)
 
-    X = lsa.fit_transform(X)
+	X = lsa.fit_transform(X)
 
-    print("done in %fs" % (time() - t0))
+	print("done in %fs" % (time() - t0))
 
-    explained_variance = svd.explained_variance_ratio_.sum()
-    print("Explained variance of the SVD step: {}%".format(
-        int(explained_variance * 100)))
+	explained_variance = svd.explained_variance_ratio_.sum()
+	print("Explained variance of the SVD step: {}%".format(
+		int(explained_variance * 100)))
 
-    print()
+	print()
 
 
 ###############################################################################
 # Do the actual clustering
 
 if opts.minibatch:
-    km = MiniBatchKMeans(n_clusters=true_k, init='k-means++', n_init=1,
-                         init_size=1000, batch_size=1000, verbose=opts.verbose)
+	km = MiniBatchKMeans(n_clusters=true_k, init='k-means++', n_init=1,
+						 init_size=1000, batch_size=1000, verbose=opts.verbose)
 else:
-    km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1,
-                verbose=opts.verbose)
+	km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1,
+				verbose=opts.verbose)
 
 print("Clustering sparse data with %s" % km)
 t0 = time()
 km.fit(X)
 print("done in %0.3fs" % (time() - t0))
 print()
-
+'''
 print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
 print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
 print("V-measure: %0.3f" % metrics.v_measure_score(labels, km.labels_))
 print("Adjusted Rand-Index: %.3f"
-      % metrics.adjusted_rand_score(labels, km.labels_))
+	  % metrics.adjusted_rand_score(labels, km.labels_))
 print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(X, km.labels_, sample_size=60))
-
+	  % metrics.silhouette_score(X, km.labels_, sample_size=60))
+'''
 print()
 
 
 if not opts.use_hashing:
-    print("Top terms per cluster:")
+	print("Top terms per cluster:")
 
-    if opts.n_components:
-        original_space_centroids = svd.inverse_transform(km.cluster_centers_)
-        order_centroids = original_space_centroids.argsort()[:, ::-1]
-    else:
-        order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+	if opts.n_components:
+		original_space_centroids = svd.inverse_transform(km.cluster_centers_)
+		order_centroids = original_space_centroids.argsort()[:, ::-1]
+	else:
+		order_centroids = km.cluster_centers_.argsort()[:, ::-1]
 
-    terms = vectorizer.get_feature_names()
-    for i in range(true_k):
-        print("Cluster %d:" % i, end='')
-        for ind in order_centroids[i, :10]:
-            print(' %s' % terms[ind], end='')
-        print()
+	terms = vectorizer.get_feature_names()
+	for i in range(true_k):
+		print("Cluster %d:" % i, end='')
+		for ind in order_centroids[i, :10]:
+			print(' %s' % terms[ind], end='')
+		print()
 
 
 
 
 
 #LDA
-
-n_samples = 40
-n_features = 100
+n_samples = 10000
+n_features = 10000
 n_topics = 10
 n_top_words = 20
 
 
 def print_top_words(model, feature_names, n_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        print("Topic #%d:" % topic_idx)
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
-    print()
+	for topic_idx, topic in enumerate(model.components_):
+		print("Topic #%d:" % topic_idx)
+		print(" ".join([feature_names[i]
+						for i in topic.argsort()[:-n_top_words - 1:-1]]))
+	print()
 
 
 print("Loading dataset...")
@@ -265,8 +266,7 @@ print("done in %0.3fs." % (time() - t0))
 
 # Use tf-idf features for NMF.
 print("Extracting tf-idf features for NMF...")
-tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=n_features,
-                                   stop_words='english')
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=n_features,decode_error = 'ignore')
 t0 = time()
 tfidf = tfidf_vectorizer.fit_transform(data_samples)
 print("done in %0.3fs." % (time() - t0))
@@ -274,15 +274,15 @@ print("done in %0.3fs." % (time() - t0))
 # Use tf (raw term count) features for LDA.
 print("Extracting tf features for LDA...")
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=n_features,
-                                stop_words='english')
+								decode_error = 'ignore')
 t0 = time()
 tf = tf_vectorizer.fit_transform(data_samples)
 print("done in %0.3fs." % (time() - t0))
 
 # Fit the NMF model
 print("Fitting the NMF model with tf-idf features,"
-      "n_samples=%d and n_features=%d..."
-      % (n_samples, n_features))
+	  "n_samples=%d and n_features=%d..."
+	  % (n_samples, n_features))
 t0 = time()
 nmf = NMF(n_components=n_topics, random_state=1, alpha=.1, l1_ratio=.5).fit(tfidf)
 
@@ -293,10 +293,10 @@ tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 print_top_words(nmf, tfidf_feature_names, n_top_words)
 
 print("Fitting LDA models with tf features, n_samples=%d and n_features=%d..."
-      % (n_samples, n_features))
+	  % (n_samples, n_features))
 lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
-                                learning_method='online', learning_offset=50.,
-                                random_state=0)
+								learning_method='online', learning_offset=50.,
+								random_state=0)
 t0 = time()
 lda.fit(tf)
 print("done in %0.3fs." % (time() - t0))
