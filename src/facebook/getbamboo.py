bafb348625c89ae2f55abd63d0f2d5c47dffd8b1
inyeoplee77 <blackin77@gmail.com>
2016-03-14 11:50:39 +0900

    api request error

--- src/facebook/getbamboo.py
+++ src/facebook/getbamboo.py
@@ -3,6 +3,7 @@ import requests
 import os
 import re
 import locale
+import time
 appID = '719437504828313'
 appSecret = 'dae1d765e81c02e566d6ccaa2b6af441'
 
@@ -32,6 +33,7 @@ nonkorean = re.compile('[^ 가-힣]+')
 hashtag = re.compile('#[가-힣\w0-9\S]+')
 nomean = re.compile('[ㄱ-ㅎ]')
 print pages
+time_count = 0
 for page in pages:
 	dir = pages[page].split('/')[-1]
 	print dir
@@ -41,7 +43,12 @@ for page in pages:
 	r = requests.get(api + page + "/posts",params = para).json()
 	if 'error' in r:
 		page_errors[page] = r['error']['message']
+		if r['error']['code'] == 17:
+			if time_count < 3:
+				time.wait(600)
+				time_count += 1
 		continue
+	time_count = 0
 	while 'previos' in r['paging']:
 		r = requests.get(r['paging']['previous']).json()
 	while True:
@@ -59,8 +66,13 @@ for page in pages:
 				like = requests.get(api + p['id'] + '/likes?summary=true',params = para).json()
 				if 'error' in like:
 					likes_errors[p['id']] = like['error']['message']
+					if like['error']['code'] == 17:
+						if time_count < 3:
+							time.wait(600)
+							time_count += 1
 					continue
 				else:
+					time_count = 0
 					likes[p['id']] = like['summary']['total_count']
 			except ValueError as v:
 				print 'returned value is not json:' + v.strerror
@@ -84,17 +96,17 @@ for page in pages:
 			continue
 		print '%d posts scrapped' % count
 		
-		
-			
-			
 for e in page_errors:
 	print 'Error occurred while processing ' + e + ' page :' + page_errors[e]
 for e in likes_errors:
 	print 'Error occurred while obtaining likes on :' + e + ' post : ' + likes_errors[e] 
 like_file = open('likes','w')
 try:
-	for like in likes:
+	for like in likes:	
 		like_file.write(like+' ' + str(likes[like]) + '\n')
+	for like in likes_errors:
+		count = requests.get(api + likes_errors[like] + '/likes?summary=true',params = para).json()['summary']['total_count']
+		like_file.write(like+' ' + str(count) + '\n')
 except Exception as e:
 	print "Error occurred while writing like file :",
 	print e.strerror
