14d2cabbd9014f8661a33e0f0544d85477f778d5
inyeoplee77 <blackin77@gmail.com>
2016-03-16 01:21:05 +0900

    sample

--- src/sample.py
+++ src/sample.py
@@ -10,10 +10,12 @@ if os.path.exists(test_target):
 	shutil.rmtree(test_target)
 os.mkdir(train_target)
 os.mkdir(test_target)
-for dir in os.listdir(base):
+for dir in os.listdir(base):	
 	if os.path.isdir(base + dir):
+		print "copying samples of " + dir + " data..." 
 		files = random.sample(os.listdir(base + dir),2000)
 		for f in files[:1000]:
 			shutil.copyfile(base + dir + "/" + f,train_target + f)
 		for f in files[1000:]:
 			shutil.copyfile(base + dir + "/" + f,test_target + f)
+print "copy complete"
