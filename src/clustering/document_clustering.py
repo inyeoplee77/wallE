6e472c1cd7cf9b00f4d5214722678e47031a6cc0
inyeoplee77 <blackin77@gmail.com>
2016-03-23 22:11:40 +0900

    clusters

--- src/clustering/document_clustering.py
+++ src/clustering/document_clustering.py
@@ -308,11 +308,11 @@ print_top_words(lda, tf_feature_names, n_top_words)
 
 
 
+#number of clusters
+clusters = 5
 
 
-
-
-km = KMeans(n_clusters=2, init='k-means++', max_iter=100, n_init=10,verbose=opts.verbose)
+km = KMeans(n_clusters=clusters, init='k-means++', max_iter=100, n_init=10,verbose=opts.verbose)
 
 result = csr_matrix(result)
 km.fit(result)
@@ -323,7 +323,7 @@ else:
 	shutil.rmtree('result')
 	os.mkdir('result')
 #order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-for i in range(2):
+for i in range(clusters):
 	print("Cluster %d:" % i, end='')
 	d = km.transform(result)[:, i]
 	ind = np.argsort(d)[::-1][:10]
